96abdfa0750e4015c737daf201b57064e1943d0a
Tino <Konstantin.v.milchev@gmail.com>
2024-03-24 08:31:12 +0000

    added custom fonts

--- results-summary-component-main/tailwind.config.js
+++ results-summary-component-main/tailwind.config.js
@@ -2,13 +2,21 @@
 module.exports = {
 	content: ['./*.html'],
 	theme: {
+		fontFamily: {
+			custom: ['med', 'sans-serif'],
+			custombold: ['bold', 'sans-serif'],
+			customextra: ['ebold', 'sans-serif'],
+			customnew: ['new', 'sans-serif'],
+		},
+
 		screens: {
 			sm: '480px',
 			md: '768px',
 			lg: '976px',
 			xl: '1440px',
 		},
-		extend: {},
+		extend: {
+		},
 	},
 	plugins: [],
 }
