b5b4ec92bbcb9b5acd881c40db0c4ab4ec4d33cf
Tino <Konstantin.v.milchev@gmail.com>
2024-03-30 22:16:45 +0000

    added smooth transition of menu

--- news-homepage-main/script.js
+++ news-homepage-main/script.js
@@ -1,20 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const openmenu = document.getElementById('openmenu')
 	const closemenu = document.getElementById('closemenu')
-	const main =document.getElementById('main')
-	const body =document.getElementById('body')
-	const side =document.getElementById('sidebar')
+	const main = document.getElementById('main')
+	const body = document.getElementById('body')
+	const side = document.getElementById('sidebar')
 
-	openmenu.addEventListener('click', function(){
+	openmenu.addEventListener('click', function() {
 		body.classList.add('bg-gray-800', 'bg-opacity-70');
-		side.classList.remove('hidden')
-		side.classList.toggle('translate-x-0')
+		side.classList.toggle('translate-x-full');
 	})
 
-	closemenu.addEventListener('click', function(){
+	closemenu.addEventListener('click', function() {
 		body.classList.remove('bg-gray-800', 'bg-opacity-70');
-		side.classList.add('hidden')
-		side.classList.toggle('translate-x-0')
+		side.classList.toggle('translate-x-full');
 	})
 }
 )
