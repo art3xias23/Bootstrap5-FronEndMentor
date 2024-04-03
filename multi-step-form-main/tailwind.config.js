92251fe6e4b38746e711495ea93cba580a73676d
Tino <Konstantin.v.milchev@gmail.com>
2024-04-03 09:00:24 +0100

    wip on mobile layout

--- multi-step-form-main/tailwind.config.js
+++ multi-step-form-main/tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html"],
-  theme: {
+	  theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
     extend: {},
   },
   plugins: [],
